581ee2ae29520bb64f0e4cae81dfbb7614aeff45
Fakeur <Fakeur@DESKTOP-TCBOG55>
2018-03-16 18:56:08 +0100

    Add project files.

---
+++ Vol3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Vol3.Startup))]
+namespace Vol3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
